83889fd2800dc0a51fcb1a14293a4b6acbd2a489
Aditya <aditya.verma@nucleusteq.com>
2025-05-20 16:49:12 +0530

    Inter-Service-Communication

--- src/Day7/gradle/src/main/java/com/example/gradle/feignClient/EmailFeignClient.java
+++ src/Day7/gradle/src/main/java/com/example/gradle/feignClient/EmailFeignClient.java
@@ -1,4 +1,4 @@
-package com.example.gradle.fiegnClient;
+package com.example.gradle.feignClient;
 import com.example.gradle.DTO.EmailDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
